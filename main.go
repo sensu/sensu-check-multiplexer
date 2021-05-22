e92e5999e73c53d4bc11ee21c7ca69d57a56d0bb
Jef Spaleta <jspaleta@gmail.com>
2021-05-21 16:12:39 -0800

    working event generation and summary report

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"log"
+	"net/http"
 	"os"
 	"os/exec"
 	"path"
 	"strings"
+	"sync"
 
 	"github.com/sensu-community/sensu-plugin-sdk/sensu"
 	"github.com/sensu/sensu-go/types"
@@ -19,7 +22,7 @@ type Config struct {
 	CommonSuffixArgs string
 	CommonPrefixArgs string
 	AnnotationPrefix string
-	CheckPrefix      string
+	CheckNamePrefix  string
 	EventsAPI        string
 	EventCheck       string
 	EventEntity      string
@@ -27,7 +30,17 @@ type Config struct {
 	DryRun           bool
 }
 
+type Command struct {
+	CommandString string
+	Status        int
+	Output        string
+	CheckName     string
+	CommandError  error
+	EventError    error
+}
+
 var (
+	commands    []*Command
 	argGroupMap = map[string]map[string]string{}
 	argsMap     = map[string]string{}
 	useStdin    = false
@@ -68,12 +81,12 @@ var (
 			Value:     &plugin.AnnotationPrefix,
 		},
 		&sensu.PluginConfigOption{
-			Path:     "event-check-prefix",
+			Path:     "check-name-prefix",
 			Env:      "MULTIPLEX_CHECK_NAME_PREFIX",
 			Argument: "check-name-prefix",
 			Default:  "multiplex_",
 			Usage:    "prefix string to use in check name for each annotation group. (Optional)",
-			Value:    &plugin.CheckPrefix,
+			Value:    &plugin.CheckNamePrefix,
 		},
 		&sensu.PluginConfigOption{
 			Path:     "event-entity",
@@ -143,26 +156,85 @@ func checkArgs(event *types.Event) (int, error) {
 	if len(plugin.AnnotationPrefix) == 0 {
 		return sensu.CheckStateWarning, fmt.Errorf("--annotation-prefix or MULTIPLEX_ANNOTATION_PREFIX environment variable is required")
 	}
-	fmt.Printf("Fields: %q\n", fields)
-	fmt.Printf("Command: %s Args: %s\n", plugin.Command, plugin.CommonPrefixArgs)
+	if plugin.DryRun {
+		fmt.Printf("Command: %s Common Args: %s\n", plugin.Command, plugin.CommonPrefixArgs)
+	}
 	return sensu.CheckStateOK, nil
 }
 
 func executeCheck(event *types.Event) (int, error) {
-	log.Println("executing check with --command", plugin.Command)
-	log.Println("event", event)
+	if plugin.DryRun {
+		fmt.Printf("\nDry-run Output\n")
+	}
+	//Populate the argsMap that forms the basis of commands to execute
 	err := createCommandlines(event)
 	if err != nil {
 		return sensu.CheckStateCritical, err
 	}
-	for group, args := range argsMap {
-		fmt.Printf("executing Command: %s Args: %s\n", plugin.Command, args)
-		fmt.Printf("exeucting Group: %s Command: %s Args: %s\n", group, plugin.Command, args)
-		cmd := exec.Command(plugin.Command, strings.Fields(args)...)
-		stdoutStderr, err := cmd.CombinedOutput()
-		fmt.Printf("Group: %s Command: %s Args: %s\n Output: %s\n Err: %v\n", group, plugin.Command, args, stdoutStderr, err)
+	// Execute commands in parallel
+	var wg sync.WaitGroup
+	for group := range argsMap {
+		wg.Add(1)
+		go func(group string, wg *sync.WaitGroup) {
+			// run command
+			c := new(Command)
+			c.Run(group)
+			// generate event
+			c.EventError = c.generateEvent(event)
+			// append command into commands array
+			commands = append(commands, c)
+			defer wg.Done()
+		}(group, &wg)
+	}
+	// Wait for all commands to finish executing
+	wg.Wait()
+	if plugin.DryRun {
+		fmt.Printf("\n\nNormal Output\n")
+	}
+	// Build Output Summary
+	for _, c := range commands {
+		fmt.Printf("Event For Command: %s Error: %v\n", c.CommandString, c.EventError)
 	}
 	return sensu.CheckStateOK, nil
+}
+func (c *Command) Run(group string) {
+	args := argsMap[group]
+	cmd := exec.Command(plugin.Command, strings.Fields(args)...)
+	output, err := cmd.CombinedOutput()
+	c.CommandString = cmd.String()
+	c.CommandError = err
+	c.CheckName = fmt.Sprintf("%s%s", plugin.CheckNamePrefix, group)
+	c.Output = string(output)
+	c.Status = cmd.ProcessState.ExitCode()
+	if plugin.DryRun {
+		fmt.Printf("Ran Command: %#v\n", c)
+	}
+
+}
+
+func (c *Command) generateEvent(event *types.Event) error {
+	event.Check.Name = c.CheckName
+	event.Check.Status = uint32(c.Status)
+	event.Check.Output = c.Output
+	eventJSON, err := json.Marshal(event)
+	if err != nil {
+		fmt.Printf("Create event failed with error %s\n", err)
+		return err
+	}
+	if plugin.DryRun {
+		fmt.Printf("Event For Command: %s\n", c.CommandString)
+		fmt.Printf("  Check Name: %s\n", event.Check.Name)
+		fmt.Printf("  Check Status: %v\n", event.Check.Status)
+		fmt.Printf("  Check Output: %s\n", event.Check.Output)
+		fmt.Printf("  Event API: %s\n  Event Data: %s\n", plugin.EventsAPI, string(eventJSON))
+	} else {
+		_, err = http.Post(plugin.EventsAPI, "application/json", bytes.NewBuffer(eventJSON))
+		if err != nil {
+			fmt.Printf("The HTTP request to create event failed with error %s\n", err)
+			return err
+		}
+	}
+	return nil
 }
 
 func createCommandlines(event *types.Event) error {
@@ -177,7 +249,9 @@ func createCommandlines(event *types.Event) error {
 		return nil
 	}
 	prefix := path.Join(plugin.AnnotationPrefix) + "/"
-	fmt.Printf("Prefix: %v\n", prefix)
+	if plugin.DryRun {
+		fmt.Printf("Annotation Prefix: %v\n", prefix)
+	}
 	if event == nil {
 		return nil
 	}
@@ -187,7 +261,9 @@ func createCommandlines(event *types.Event) error {
 	if event.Entity != nil {
 		processAnnotations(event.Entity.Annotations, "Entity", prefix)
 	}
-	fmt.Printf("Final Annotation Map: %q\n", argGroupMap)
+	if plugin.DryRun {
+		fmt.Printf("Final Annotation Map: %q\n", argGroupMap)
+	}
 	//loop over argument groups
 	for group, arguments := range argGroupMap {
 		// start with common prefix args stripped from the command
@@ -223,21 +299,27 @@ func processAnnotations(annotations map[string]string, annotationSource string, 
 					group = subpath[0]
 					groupMap := argGroupMap[group]
 					if groupMap == nil { // initialize map
-						fmt.Printf("%s initialize Map: %v\n", annotationSource, group)
+						if plugin.DryRun {
+							fmt.Printf("%s initialize Map: %v\n", annotationSource, group)
+						}
 						argGroupMap[group] = map[string]string{}
 					}
 					if group == path {
 						argsMap[group] = value
-						fmt.Printf("%s annotation: Group: %v Args Value: %v\n",
-							annotationSource, group, argsMap[group])
+						if plugin.DryRun {
+							fmt.Printf("%s annotation: Group: %v Args Value: %v\n",
+								annotationSource, group, argsMap[group])
+						}
 					}
 				}
 				if len(subpath) == 2 {
 					opt = subpath[1]
 					if len(group) > 0 && len(opt) > 0 {
 						argGroupMap[group][opt] = value
-						fmt.Printf("%s annotation: Group: %v Opt: %v Value: %v\n",
-							annotationSource, group, opt, argGroupMap[group][opt])
+						if plugin.DryRun {
+							fmt.Printf("%s annotation: Group: %v Opt: %v Value: %v\n",
+								annotationSource, group, opt, argGroupMap[group][opt])
+						}
 					}
 				}
 			}
